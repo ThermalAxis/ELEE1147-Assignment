abd7a9b05789add0136c33ae63c9fe6c8c14cb8b
Connor <25871918+ThermalAxis@users.noreply.github.com>
2024-02-12 14:59:42 +0000

    added csv export functionality

--- ELEE1147_Antartic_Weather/exportCSV.h
+++ ELEE1147_Antartic_Weather/exportCSV.h
@@ -2,6 +2,10 @@
 #ifndef EXPORT_CSV_H
 #define EXPORT_CSV_H
 
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
 #include "json_parser.h" // YOU CAN ADD OTHER HEADERS UNDERNEATH OR ABOVE
 
 void exportCSV(TelemetryData* telemetryArray, int arraySize);
