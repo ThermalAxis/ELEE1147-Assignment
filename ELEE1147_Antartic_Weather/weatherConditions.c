16adc92463da41a1d7d9d7bc60cab7e1b88610d8
Connor <25871918+ThermalAxis@users.noreply.github.com>
2024-02-26 12:57:03 +0000

    merged changes

--- ELEE1147_Antartic_Weather/weatherConditions.c
+++ ELEE1147_Antartic_Weather/weatherConditions.c
@@ -3,10 +3,18 @@
 #include <string.h>
 
 void weatherConditions(TelemetryData* telemetryArray, int arraySize) {
+<<<<<<< Updated upstream
 	system("clear"); // clear the terminal
 
     printf("Weather Conditions\n\n");
 
 	for (int i = 0; i < arraySize; ++i) {
+=======
+	//system("clear"); // clear the terminal
+
+ //   printf("Weather Conditions\n\n");
+
+	//for (int i = 0; i < arraySize; ++i) {
+>>>>>>> Stashed changes
 	
 	}
