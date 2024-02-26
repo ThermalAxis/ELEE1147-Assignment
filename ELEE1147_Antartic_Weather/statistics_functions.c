b4f40c27e7066dcfff100b045e2e5b62bbae34c6
Connor <25871918+ThermalAxis@users.noreply.github.com>
2024-02-26 12:59:56 +0000

    merged changes

--- ELEE1147_Antartic_Weather/statistics_functions.c
+++ ELEE1147_Antartic_Weather/statistics_functions.c
@@ -1,7 +1,3 @@
 #include "statistics_functions.h"
 #include <stdio.h>
 #include <string.h>
-
-void weatherConditions(TelemetryData *telemetryArray, int arraySize) {
-  printf("There's nothing here yet.\n");
-}
