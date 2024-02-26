09d97f0008cc1a67f52da002c348bee4fe5cc524
Connor <25871918+ThermalAxis@users.noreply.github.com>
2024-02-26 14:10:25 +0000

    formatted exportCSV.c

--- ELEE1147_Antartic_Weather/exportCSV.c
+++ ELEE1147_Antartic_Weather/exportCSV.c
@@ -20,7 +20,7 @@ void exportCSV(TelemetryData *telemetryArray, int arraySize) {
 
   if (exportSelect == 'n' || exportSelect == 'N') {
     // Write all data to the output file
-    
+
     printf("Writing data...");
     if (fprintf(outputFile, "Timestamp, sensorID, SensorType, Location, "
                             "Measurement, Status\n") < 0) {
@@ -40,192 +40,131 @@ void exportCSV(TelemetryData *telemetryArray, int arraySize) {
     printf("\nCSV file export successful!\n\n");
 
     return 0; // Exit successfully
-  }
-  else if (exportSelect == 'y' || exportSelect == 'Y') {
-      // filter data and export to file.
-      int exportFilter;
-      printf("Select a filter type:\n");
-      printf("1 - Filter by Location\n");
-      printf("2 - Filter by Sensor Type\n");
-      printf("3 - Filter by Sensor ID\n");
+  } else if (exportSelect == 'y' || exportSelect == 'Y') {
+    // filter data and export to file.
+    int exportFilter;
+    printf("Select a filter type:\n");
+    printf("1 - Filter by Location\n");
+    printf("2 - Filter by Sensor Type\n");
+    printf("3 - Filter by Sensor ID\n");
 
-      scanf("%d", &exportFilter);
+    scanf("%d", &exportFilter);
 
-      switch (exportFilter) {
-      default:
-          printf("Invalid selection.");
-          return;
-      case 0:
-          printf("Invalid selection.");
-          return;
-      case 1:
-          printf("Filter by Location.\n");
-          char locationName[34];
-          printf("Enter the Location name to filter: ");
-
-#if (_MSC_VER > 1600)
-          while (getchar() != '\n'); // clear previous carriage return
-          scanf_s("%33[^\n]", locationName, (unsigned)(sizeof(locationName) / sizeof(locationName[0])));
-#else
-          scanf("%33[^\n]", locationName);
-#endif
-
-          printf("Telemetry Data for Location '%s':\n", locationName);
-          printf("Writing data...");
-          if (fprintf(outputFile, "Timestamp, sensorID, SensorType, Location, "
-              "Measurement, Status\n") < 0) {
-              // Handle output error using perror()
-              perror("Error writing output to file");
-              fclose(outputFile); // Close the output file before exiting
-              return 1;           // Exit with an error code
-          }
-          else {
-              for (int i = 0; i < arraySize; ++i) {
-                  if (strcmp(telemetryArray[i].location, locationName) == 0) {
-                      fprintf(outputFile, "%s,%s,%s,%s,%.2f,%s\n",
-                          telemetryArray[i].timestamp, telemetryArray[i].sensorID,
-                          telemetryArray[i].sensorType, telemetryArray[i].location,
-                          telemetryArray[i].measurement, telemetryArray[i].status);
-                  }
-              }
-              fclose(outputFile); // Close outputFile
-              printf("\nCSV file export successful!\n\n");
-              return;
-          }
-          return;
-
-
-      case 2:
-          printf("Filter by Sensor Type.\n");
-          char sensorType[20];
-          printf("Enter the sensor type to filter: ");
+    switch (exportFilter) {
+    default:
+      printf("Invalid selection.");
+      return;
+    case 0:
+      printf("Invalid selection.");
+      return;
+    case 1:
+      printf("Filter by Location.\n");
+      char locationName[34];
+      printf("Enter the Location name to filter: ");
 
 #if (_MSC_VER > 1600)
-          scanf_s("%19s", sensorType, (unsigned)(sizeof(sensorType) / sizeof(sensorType[0])));
+      while (getchar() != '\n')
+        ; // clear previous carriage return
+      scanf_s("%33[^\n]", locationName,
+              (unsigned)(sizeof(locationName) / sizeof(locationName[0])));
 #else
-          scanf("%19s", sensorType);
+      scanf("%33[^\n]", locationName);
 #endif
 
-          printf("Telemetry Data for Sensor Type '%s':\n", sensorType);
-          printf("Writing data...");
-          if (fprintf(outputFile, "Timestamp, sensorID, SensorType, Location, "
-              "Measurement, Status\n") < 0) {
-              // Handle output error using perror()
-              perror("Error writing output to file");
-              fclose(outputFile); // Close the output file before exiting
-              return 1;           // Exit with an error code
+      printf("Telemetry Data for Location '%s':\n", locationName);
+      printf("Writing data...");
+      if (fprintf(outputFile, "Timestamp, sensorID, SensorType, Location, "
+                              "Measurement, Status\n") < 0) {
+        // Handle output error using perror()
+        perror("Error writing output to file");
+        fclose(outputFile); // Close the output file before exiting
+        return 1;           // Exit with an error code
+      } else {
+        for (int i = 0; i < arraySize; ++i) {
+          if (strcmp(telemetryArray[i].location, locationName) == 0) {
+            fprintf(outputFile, "%s,%s,%s,%s,%.2f,%s\n",
+                    telemetryArray[i].timestamp, telemetryArray[i].sensorID,
+                    telemetryArray[i].sensorType, telemetryArray[i].location,
+                    telemetryArray[i].measurement, telemetryArray[i].status);
           }
-          else {
-              for (int i = 0; i < arraySize; ++i) {
-                  if (strcmp(telemetryArray[i].sensorType, sensorType) == 0) {
-                      fprintf(outputFile, "%s,%s,%s,%s,%.2f,%s\n",
-                          telemetryArray[i].timestamp, telemetryArray[i].sensorID,
-                          telemetryArray[i].sensorType, telemetryArray[i].location,
-                          telemetryArray[i].measurement, telemetryArray[i].status);
-                  }
-              }
-              fclose(outputFile); // Close outputFile
-              printf("\nCSV file export successful!\n\n");
-              return;
-          }
-
-      case 3:
-          printf("Filter by Sensor ID.\n");
-          char sensorID[10];
-          printf("Enter the sensor ID to filter: ");
-
-#if (_MSC_VER > 1600)
-          scanf_s("%9s", sensorID, (unsigned)(sizeof(sensorID) / sizeof(sensorID[0])));
-#else
-          scanf("%9s", sensorType);
-#endif
-
-          printf("Telemetry Data for Sensor ID '%s':\n", sensorID);
-          printf("Writing data...");
-          if (fprintf(outputFile, "Timestamp, sensorID, SensorType, Location, "
-              "Measurement, Status\n") < 0) {
-              // Handle output error using perror()
-              perror("Error writing output to file");
-              fclose(outputFile); // Close the output file before exiting
-              return 1;           // Exit with an error code
-          }
-          else {
-              for (int i = 0; i < arraySize; ++i) {
-                  if (strcmp(telemetryArray[i].sensorID, sensorID) == 0) {
-                      fprintf(outputFile, "%s,%s,%s,%s,%.2f,%s\n",
-                          telemetryArray[i].timestamp, telemetryArray[i].sensorID,
-                          telemetryArray[i].sensorType, telemetryArray[i].location,
-                          telemetryArray[i].measurement, telemetryArray[i].status);
-                  }
-              }
-              fclose(outputFile); // Close outputFile
-              printf("\nCSV file export successful!\n\n");
-
-              return;
-          }
+        }
+        fclose(outputFile); // Close outputFile
+        printf("\nCSV file export successful!\n\n");
+        return;
       }
+      return;
+
+    case 2:
+      printf("Filter by Sensor Type.\n");
+      char sensorType[20];
+      printf("Enter the sensor type to filter: ");
+
+#if (_MSC_VER > 1600)
+      scanf_s("%19s", sensorType,
+              (unsigned)(sizeof(sensorType) / sizeof(sensorType[0])));
+#else
+      scanf("%19s", sensorType);
+#endif
+
+      printf("Telemetry Data for Sensor Type '%s':\n", sensorType);
+      printf("Writing data...");
+      if (fprintf(outputFile, "Timestamp, sensorID, SensorType, Location, "
+                              "Measurement, Status\n") < 0) {
+        // Handle output error using perror()
+        perror("Error writing output to file");
+        fclose(outputFile); // Close the output file before exiting
+        return 1;           // Exit with an error code
+      } else {
+        for (int i = 0; i < arraySize; ++i) {
+          if (strcmp(telemetryArray[i].sensorType, sensorType) == 0) {
+            fprintf(outputFile, "%s,%s,%s,%s,%.2f,%s\n",
+                    telemetryArray[i].timestamp, telemetryArray[i].sensorID,
+                    telemetryArray[i].sensorType, telemetryArray[i].location,
+                    telemetryArray[i].measurement, telemetryArray[i].status);
+          }
+        }
+        fclose(outputFile); // Close outputFile
+        printf("\nCSV file export successful!\n\n");
+        return;
+      }
+
+    case 3:
+      printf("Filter by Sensor ID.\n");
+      char sensorID[10];
+      printf("Enter the sensor ID to filter: ");
+
+#if (_MSC_VER > 1600)
+      scanf_s("%9s", sensorID,
+              (unsigned)(sizeof(sensorID) / sizeof(sensorID[0])));
+#else
+      scanf("%9s", sensorType);
+#endif
+
+      printf("Telemetry Data for Sensor ID '%s':\n", sensorID);
+      printf("Writing data...");
+      if (fprintf(outputFile, "Timestamp, sensorID, SensorType, Location, "
+                              "Measurement, Status\n") < 0) {
+        // Handle output error using perror()
+        perror("Error writing output to file");
+        fclose(outputFile); // Close the output file before exiting
+        return 1;           // Exit with an error code
+      } else {
+        for (int i = 0; i < arraySize; ++i) {
+          if (strcmp(telemetryArray[i].sensorID, sensorID) == 0) {
+            fprintf(outputFile, "%s,%s,%s,%s,%.2f,%s\n",
+                    telemetryArray[i].timestamp, telemetryArray[i].sensorID,
+                    telemetryArray[i].sensorType, telemetryArray[i].location,
+                    telemetryArray[i].measurement, telemetryArray[i].status);
+          }
+        }
+        fclose(outputFile); // Close outputFile
+        printf("\nCSV file export successful!\n\n");
+
+        return;
+      }
+    }
   } else {
     printf("Invalid input, returning to main menu.\n\n");
     return 0;
   }
 }
-
- //void exportBySensorType(TelemetryData* telemetryArray, int arraySize, FILE*
- //outputFile) {
- //    
- //}
-
-// void exportBySensorID(TelemetryData* telemetryArray, int arraySize, FILE
-// *outputFile) {
-//     char sensorID[10];
-//     printf("Enter the Sensor ID to filter: ");
-//
-// #if (_MSC_VER > 1600)
-//     scanf_s("%9s", sensorID, (unsigned)(sizeof(sensorID) /
-//     sizeof(sensorID[0])));
-// #else
-//     scanf("%9s", sensorID);
-// #endif
-//
-//     printf("Telemetry Data for Sensor ID '%s':\n", sensorID);
-//     for (int i = 0; i < arraySize; ++i) {
-//         if (strcmp(telemetryArray[i].sensorID, sensorID) == 0) {
-//             printf("SensorID: %s, SensorType: %s, Location: %s, Measurement:
-//             %.2f, "
-//                 "Status: %s\n",
-//                 telemetryArray[i].sensorID, telemetryArray[i].sensorType,
-//                 telemetryArray[i].location, telemetryArray[i].measurement,
-//                 telemetryArray[i].status);
-//         }
-//     }
-//     return;
-// }
-//
-// void exportByLocation(TelemetryData* telemetryArray, int arraySize, FILE
-// *outputFile) {
-//     char locationName[34];
-//     printf("Enter the Location to filter: ");
-//
-// #if (_MSC_VER > 1600)
-//     while (getchar() != '\n')
-//         ; // clear previous carriage return
-//     scanf_s("%33[^\n]", locationName,
-//         (unsigned)(sizeof(locationName) / sizeof(locationName[0])));
-// #else
-//     scanf("%33[^\n]", locationName);
-// #endif
-//
-//     printf("Telemetry Data for Location '%s':\n", locationName);
-//     for (int i = 0; i < arraySize; ++i) {
-//         if (strcmp(telemetryArray[i].location, locationName) == 0) {
-//             printf("Timestamp: %s, SensorID: %s, SensorType: %s, Location:
-//             %s, "
-//                 "Measurement: %.2f, "
-//                 "Status: %s\n",
-//                 telemetryArray[i].timestamp, telemetryArray[i].sensorID,
-//                 telemetryArray[i].sensorType, telemetryArray[i].location,
-//                 telemetryArray[i].measurement, telemetryArray[i].status);
-//         }
-//     }
-//     return;
-// }
